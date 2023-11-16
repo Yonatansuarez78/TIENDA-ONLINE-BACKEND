8825fc812bc020557a22d19e5fcdc2cdad0baa19
yonatan78 <yonatansuarez78@gmail.com>
2023-11-15 19:40:04 -0500

    modificacion

--- client/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import axios from './axios'
-
-const API = 'http://localhost:4000/api'
-
-export const registerRequest = user => axios.post(`/register`, user)
-
-export const loginRequest = user => axios.post(`/login`, user)
-
-export const verityTokenRequest = () => axios.get(`/verify`)
