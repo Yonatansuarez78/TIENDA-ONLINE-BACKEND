f0e1eeaf291b2d2af633441bc784d5bab0c9c035
yonatan78 <yonatansuarez78@gmail.com>
2023-11-17 19:49:37 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const app = express()
 // });
 
 app.use(cors({
-    // origin: 'http://localhost:5173',
-    origin: '*',
+    origin: 'http://localhost:5173',
+    // origin: '*',
     credentials: true
 }))
 app.use(morgan('dev'))
