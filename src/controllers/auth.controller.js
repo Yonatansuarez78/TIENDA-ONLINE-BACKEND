9b77b4e5cc27af2ce078190cf6b913137e3d3341
Yonatan Suarez <yonatansuarez78@gmail.com>
2025-07-17 17:06:06 -0500

    1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -56,7 +56,13 @@ export const login = async (req, res) => {
 
         const token = await createAcessToken({ id: userFound._id });
 
-        res.cookie('token', token);
+        // res.cookie('token', token);
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true, // obligatorio en https (Vercel lo es)
+            sameSite: 'none', // necesario cuando tu frontend y backend son diferentes dominios
+        });
+
         res.json({
             id: userFound._id,
             username: userFound.username,
